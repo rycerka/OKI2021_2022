02207da66fa420d84056407a1eda45fdab203faf
rycerka <102176779+rycerka@users.noreply.github.com>
2022-03-29 17:55:33 +0200

    Added ios_base::stdio(0)
    Added ios_base::stdio(0), cin.tie(0) and cout.tie(0).

--- prz.cpp
+++ prz.cpp
@@ -2,34 +2,37 @@
 using namespace std;
 
 const int MAX_N = 1e6;
-int buttnos[MAX_N + 6];
+int buttons[MAX_N + 6];
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int n, m;
     int p;
     int max_new = 0;
-    int max_old = 0; //max when pushed the n + 1 button
+    int max_old = 0; //max when the n + 1 button pushed 
     cin >> n >> m;
     for (int i = 0; i < m; ++i) {
         cin >> p;
         if (p != n+1) {
-            if (buttnos[p] < max_old) {
-                buttnos[p] = max_old + 1;
+            if (buttons[p] < max_old) {
+                buttons[p] = max_old + 1;
             } else {
-                buttnos[p]++;
+                buttons[p]++;
             }
-            if (max_new < buttnos[p]) {
-                max_new = buttnos[p];
+            if (max_new < buttons[p]) {
+                max_new = buttons[p];
             }
         } else {
             max_old = max_new;
         }
     }
     for (int i = 1; i <= n; ++i) {
-        if (buttnos[i] < max_old) {
-            buttnos[i] = max_old;
+        if (buttons[i] < max_old) {
+            buttons[i] = max_old;
         }
-        cout << buttnos[i] << " ";
+        cout << buttons[i] << " ";
     }
 
     return 0;
