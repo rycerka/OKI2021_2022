1bae980ee1d11d2809cceeafe322e4ca07fe3579
rycerka <102176779+rycerka@users.noreply.github.com>
2022-04-11 15:57:49 +0200

    Add files via upload

---
+++ print_server.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class mqueue {
+public:
+    mqueue() {
+        first = last = -1;
+    }
+    bool empty() {
+        if (first == -1) {
+            return true;
+        }
+        return false;
+    }
+    int size() {
+        if (first == -1) {
+            return 0;
+        }
+        return last - first + 1;
+    }
+    void push(int value) {
+        if (empty()) {
+            elements.push_back(value);
+            first = last = 0;
+            return;
+        }
+        elements.push_back(value);
+        ++last;
+    }
+    void pop() {
+        if (first == last) {
+            first = last = -1;
+            elements.clear();
+        }
+        first++;
+    }
+    int front() {
+        int value;
+        if (empty()) {
+            return value;
+        }
+        value = elements[first];
+        return value;
+    }
+    int dequeue() {
+        int value;
+        if (empty()) {
+            return value;
+        }
+        if (first == last) {
+            value = elements[first];
+            first = last = -1;
+            elements.clear();
+            return value;
+        }
+        value = elements[first];
+        first++;
+        return value;
+    }
+private:
+    vector<int> elements;
+    int first, last;
+
+};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    mqueue printer;
+    int n, number_of_so_far_prints = 0, total_print_time = 0, average_print_time = -1, expected_time, start_time, print_time;
+    int longest_print = 0, longest_queue = 0;
+    char request;
+    int time;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> request;
+        switch (request) {
+            case 'd':
+                cin >> time;
+                printer.push(time);
+                if (average_print_time == -1) {
+                    expected_time = -1;
+                } else {
+                    expected_time = printer.size() * average_print_time;
+                }
+                longest_queue = max(longest_queue, printer.size());
+                cout << "Request accepted. You are number " << printer.size() << " in queue, ";
+                cout << "expected time is " << expected_time << " seconds.\n";
+                break;
+            case 'g':
+                cin >> time;
+                if (printer.empty()) {
+                    cout << "Printer is ready.\n";
+                    break;
+                }
+                start_time = printer.dequeue();
+                print_time = time - start_time;
+                total_print_time += print_time;
+                number_of_so_far_prints++;
+                average_print_time = total_print_time / number_of_so_far_prints;
+                longest_print = max(longest_print, print_time);
+                cout << "Printer is ready. Print time: " << print_time << " seconds.\n";
+                break;
+            case 's':
+                cout << "Stats: Longest queue: " << longest_queue << ", longest waiting time: " << longest_print << " seconds.\n";
+                break;
+        }
+    }
+    return 0;
+}
